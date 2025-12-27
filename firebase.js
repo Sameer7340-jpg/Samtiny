c4dcaebcf188c363dca6f2fde74ebf659999f9b7
Sameer7340-jpg <aveskha11@gmail.com>
2025-12-27 23:40:00 +0530

    Update Firebase imports and configuration

--- firebase.js
+++ firebase.js
@@ -1,12 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyBgmKxKLsyOqq7ZTdvvsD8aT2_wcgDpmHs",
   authDomain: "samtiny-auth.firebaseapp.com",
   projectId: "samtiny-auth",
-  appId: "YOUR_APP_ID"
+  storageBucket: "samtiny-auth.firebasestorage.app",
+  messagingSenderId: "791323731204",
+  appId: "1:791323731204:web:8fa5906c4663ca7c5c1c51"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
