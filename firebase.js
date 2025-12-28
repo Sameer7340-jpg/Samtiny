ed74ac99cd69bd88849d7cc6d500c09edb6fc502
Sameer7340-jpg <aveskha11@gmail.com>
2025-12-28 10:29:50 +0530

    Update Firebase SDK imports and configuration
    Updated Firebase SDK imports to use specific version URLs and modified configuration values to placeholders.

--- firebase.js
+++ firebase.js
@@ -1,17 +1,12 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBgmKxKLsyOqq7ZTdvvsD8aT2_wcgDpmHs",
+  apiKey: "PASTE_YOUR_API_KEY",
   authDomain: "samtiny-auth.firebaseapp.com",
   projectId: "samtiny-auth",
-  storageBucket: "samtiny-auth.firebasestorage.app",
-  messagingSenderId: "791323731204",
-  appId: "1:791323731204:web:8fa5906c4663ca7c5c1c51"
+  appId: "PASTE_YOUR_APP_ID"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
